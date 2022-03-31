3e8405dff9354397e2431d8982b03483bd6f9b36
Gloria Sekyere <gloriasekyere15@gmail.com>
2022-03-31 14:36:13 +0000

    Complete Header component styling for light and dark mode

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,31 @@ import { useMode } from '../hooks/useMode'
 
 // assets
 import LightMode from '../assets/lightMode.svg'
+import DarkMode from '../assets/darkMode.svg'
 
 //styles
 import './Header.css';
 
 
 function Header() {
-  const { toggleMode } = useMode()
+  const { mode, toggleMode } = useMode()
 
   return (
     <div className='header'>
       <h1>Where in the world?</h1>
       <div className='mode-selector' onClick={() => toggleMode()}>
-        <img src={LightMode} alt="light-mode-icon" />
-        <span>Dark Mode</span>
+        {mode === 'light' ? (
+            <>
+              <img src={LightMode} alt="light-mode-icon" />
+              <span>Dark Mode</span>
+            </>
+          ) : (
+            <>
+              <img src={DarkMode} alt="dark-mode-icon" />
+              <span>Light Mode</span>
+            </>
+          )
+        }
       </div>
     </div>
   )
