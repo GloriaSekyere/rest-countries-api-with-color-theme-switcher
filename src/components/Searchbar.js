6d6eb66bf91069dd4f52c168c94feb1e280e5da8
Gloria Sekyere <gloriasekyere15@gmail.com>
2022-04-02 20:40:02 +0000

    output template for searchbar component, when search icon is clicked, direct user to the search page with the search query in the url

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,9 +1,42 @@
+import { useMode } from '../hooks/useMode';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+//assets 
+import SearchLight from '../assets/search_light.svg';
+import SearchDark from '../assets/search_dark.svg';
+
+//styles
 import styles from './Searchbar.module.css';
 
 
 function Searchbar() {
+  const { mode } = useMode()
+  const [search, setSearch] = useState('')
+  const history = useHistory()
+
+  const handleSearch = () => {
+    history.push(`/search/q=${search}`)
+  }
+
   return (
-    <div>Searchbar</div>
+    <label className={styles.search}>
+      <div 
+        className={styles['search-icon']}
+        onClick={handleSearch}
+      >
+        <img
+          src={mode === 'light' ? SearchLight : SearchDark}
+          alt="search-icon" 
+        />
+      </div>
+      <input
+        className={styles['search-input']}
+        type='text'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+    </label>
   )
 }
 
