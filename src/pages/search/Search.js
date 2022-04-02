881ba3ee7e4d302caee36deeadc9bf3cd4dd383b
Gloria Sekyere <gloriasekyere15@gmail.com>
2022-04-02 21:01:22 +0000

    use useLocation to get query string and use querystring to fetch data matching the query

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,8 +1,29 @@
+import { useLocation } from 'react-router-dom';
+import { useFetch } from '../../hooks/useFetch';
+
+//components
+import BackToHome from '../../components/BackToHome';
+import CountryList from '../../components/CountryList';
+
+//styles
 import styles from './Search.module.css';
 
 function Search() {
+  const queryString = useLocation().search
+  const queryParams = new URLSearchParams(queryString)
+  const query = queryParams.get('q')
+
+  console.log(query)
+  const url = `https://restcountries.com/v2/name/${query}`;
+  const {data:countries, isPending, error} = useFetch(url)
+
   return (
-    <div>Search</div>
+    <div>
+      <BackToHome />
+      {isPending && <p>Loading countries...</p>}
+      {error && <p>Uh oh, could not load countries...</p>}
+      {countries && <CountryList countries={countries} />}
+    </div>
   )
 }
 
