16de0662fceb7550934aa56227d427c8a1824369
Gloria Sekyere <gloriasekyere15@gmail.com>
2022-04-02 13:28:20 +0000

    output template for necessary information for each country in CountryList component

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -4,8 +4,16 @@ function CountryList({ countries }) {
   return (
     <div>
       {countries.map(country => (
-        <div key={country.alpha3Code}>
-          {country.name}
+        <div key={country.alpha3Code} className="country-card">
+          <div className='country-flag'>
+            <img src={country.flag} alt={country.name} />
+          </div>
+          <h2 className='country-name'>{country.name}</h2>
+          <ul className='country-info-list'>
+            <li className='country-info-list-item'><span>Population:</span> {country.population}</li>
+            <li className='country-info-list-item'><span>Region:</span> {country.region}</li>
+            <li className='country-info-list-item'><span>Capital:</span> {country.capital}</li>
+          </ul>
         </div>
       ))}
     </div>
