f8e861e478e279e6d8e84b9f145824c7650583c4
Gloria Sekyere <gloriasekyere15@gmail.com>
2022-04-02 14:42:51 +0000

    Construct endpoint using name param from the url and successfully fetch country

--- src/pages/country/Country.js
+++ src/pages/country/Country.js
@@ -1,5 +1,6 @@
 import { useFetch } from '../../hooks/useFetch';
 import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 
 
 //styles
@@ -7,8 +8,14 @@ import './Country.css';
 
 function Country() {
   const { name } = useParams()
-  const url = "";
+  const url = `https://restcountries.com/v2/name/${name}?fullText=true`;
   const { data:country, isPending, error } = useFetch(url)
+
+  useEffect(() => {
+    if (country) {
+      console.log(country)
+    }
+  }, [country])
 
   return (
     <div>
@@ -16,7 +23,7 @@ function Country() {
       {error && <p>Uh oh, could not load country...</p>}
       {country && (
         <div>
-          {country.name}
+          <img src={country[0].flag} />
         </div>
       )}
     </div>
