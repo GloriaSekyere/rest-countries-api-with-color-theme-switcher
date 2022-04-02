cf2597ca090de58c08bad01e21445d13cba15697
Gloria Sekyere <gloriasekyere15@gmail.com>
2022-04-02 14:02:38 +0000

    add styling to CountryList component to properly display countries

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -8,12 +8,14 @@ function CountryList({ countries }) {
           <div className='country-flag'>
             <img src={country.flag} alt={country.name} />
           </div>
-          <h2 className='country-name'>{country.name}</h2>
-          <ul className='country-info-list'>
-            <li className='country-info-list-item'><span>Population:</span> {country.population}</li>
-            <li className='country-info-list-item'><span>Region:</span> {country.region}</li>
-            <li className='country-info-list-item'><span>Capital:</span> {country.capital}</li>
-          </ul>
+          <div className='country-info'>
+            <h2 className='country-name'>{country.name}</h2>
+            <ul className='country-info-list'>
+              <li className='country-info-list-item'><span>Population:</span> {country.population}</li>
+              <li className='country-info-list-item'><span>Region:</span> {country.region}</li>
+              <li className='country-info-list-item'><span>Capital:</span> {country.capital}</li>
+            </ul>
+          </div>
         </div>
       ))}
     </div>
