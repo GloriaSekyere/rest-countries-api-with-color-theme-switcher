f86df23da6c6bf568dedce6cec561b6fc7cb0a6d
Gloria Sekyere <gloriasekyere15@gmail.com>
2022-04-04 10:35:53 +0000

    toggle BackToTop component visibility based on scroll position

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useMode } from './hooks/useMode';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useState } from 'react';
 
 //components
 import Header from './components/Header'
@@ -9,15 +10,27 @@ import Country from './pages/country/Country';
 import BackToTop from './components/BackToTop';
 
 //styles
-//import styles from './App.module.css';
 import './App.css'
+
 
 function App() {
   const { mode } = useMode()
+  const [isBackToTopVisible, setIsBackToTopVisible] = useState(false)
+
+  const handleBackToTop = () => {
+    if (window.scrollY >= 2500){
+      setIsBackToTopVisible(true)
+    } else {
+      setIsBackToTopVisible(false)
+    }
+  }
+
+  window.addEventListener('scroll', handleBackToTop)
 
   return (
     <div className={`App ${mode}`}>
       <Header />
+
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
@@ -31,7 +44,8 @@ function App() {
           </Route>
         </Switch>
       </BrowserRouter>
-      <BackToTop />
+
+      {isBackToTopVisible && <BackToTop />}
     </div>
   );
 }
