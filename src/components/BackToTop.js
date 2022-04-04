341423ac353cc75c6827a4163df235ee48ab239a
Gloria Sekyere <gloriasekyere15@gmail.com>
2022-04-04 10:30:39 +0000

    Output template and create functionality for BackToTopcomponent

--- src/components/BackToTop.js
+++ src/components/BackToTop.js
@@ -1,9 +1,27 @@
+import { useMode } from '../hooks/useMode';
+
+//assets 
+import ArrowLight from '../assets/arrow_top_light.svg';
+import ArrowDark from '../assets/arrow_top_dark.svg';
+
+//styles
 import styles from './BackToTop.module.css';
 
 function BackToTop() {
+  const { mode } = useMode()
+
   return (
-    <div>BackToTop</div>
+    <div 
+      className={styles['to-top']}
+      onClick={() => window.scrollTo(0,0)}
+    >
+      <img
+        className={styles['to-top-arrow']}
+        src={mode === 'light' ? ArrowLight : ArrowDark} 
+        alt="back-to-top"
+      />
+    </div>
   )
 }
 
-export default BackToTop
+export default BackToTop;
