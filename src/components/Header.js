31892df35d253ca771b883290d5c9be1ad274348
Gloria Sekyere <gloriasekyere15@gmail.com>
2022-04-04 13:47:11 +0000

    reduce code for mode switch icon and text

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,18 +31,11 @@ function Header() {
     >
       <h1>Where in the world?</h1>
       <div className={styles['mode-selector']} onClick={() => toggleMode()}>
-        {mode === 'light' ? (
-            <>
-              <img src={LightMode} alt="light-mode-icon" />
-              <span>Dark Mode</span>
-            </>
-          ) : (
-            <>
-              <img src={DarkMode} alt="dark-mode-icon" />
-              <span>Light Mode</span>
-            </>
-          )
-        }
+        <img 
+          src={mode === 'light' ? LightMode : DarkMode} 
+          alt={mode === 'light' ? 'light-mode' : 'dark-mode'} 
+        />
+        <span>{mode === 'light' ? 'Dark Mode' : 'Light Mode'}</span>
       </div>
     </div>
   )
